e9b511a0b0962991ad0294ae230d35dc45e9be43
philkoerber <philipp.koerber.93@gmail.com>
2023-07-20 15:50:41 +0900

    disable 'your chats' directory (for now)

--- app/yourchats/page.js
+++ app/yourchats/page.js
@@ -1,13 +1,6 @@
 import React from 'react';
-import { cookies } from "next/headers"
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
-import { getServerSession } from 'next-auth';
-import { options } from '../api/auth/[...nextauth]/options';
 
 async function YourChats(props) {
-  const supabase = createServerComponentClient({ cookies })
-  const session = await getServerSession(options)
-  console.log(session)
     
   return (
     <div>
