21d29b3af1e3dfb10c9e52c7bf9f28ba5de0de84
philkoerber <philipp.koerber.93@gmail.com>
2023-08-20 01:11:25 +0900

    update error message

--- app/getAnswer.jsx
+++ app/getAnswer.jsx
@@ -1,37 +1,37 @@
 async function getAnswer({ question, config }) {
   console.log(question);
-  
+
   const { MEMORY_KEY, FLOWISE_URI, FLOWISE_KEY } = config;
-  
+
   const body = JSON.stringify({
-    "question": question,
-    "overrideConfig": {
-      "memoryKey": MEMORY_KEY
-    }
+    question: question,
+    overrideConfig: {
+      memoryKey: MEMORY_KEY,
+    },
   });
-  
+
   console.log("calling flowise...", FLOWISE_URI);
-  
+
   try {
     const response = await fetch(FLOWISE_URI, {
       headers: {
-        "Authorization": "Bearer " + FLOWISE_KEY,
-        "Accept": "application/json",
-        "Content-Type": 'application/json'
+        Authorization: "Bearer " + FLOWISE_KEY,
+        Accept: "application/json",
+        "Content-Type": "application/json",
       },
       method: "POST",
-      body: body
+      body: body,
     });
 
     if (!response.ok) {
-      return('ERROR in receiving answer from FlowiseAPI. Please reload the page.');
+      return "ERROR in receiving answer from FlowiseAPI. Servers and API Calls are expensive. If you want to see this app in action, contact Philipp. ";
     }
 
     const result = await response.json();
     return result;
   } catch (e) {
     console.log("Error in getAnswer:", e);
-    return "ERROR in receiving answer from FlowiseAPI. Please reload the page.";
+    return "ERROR in receiving answer from FlowiseAPI. Servers and API Calls are expensive. If you want to see this app in action, contact Philipp. ";
   }
 }
 
